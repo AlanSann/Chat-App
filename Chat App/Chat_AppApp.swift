69bfb74e592906d9346b6fbf472422a0e15aed3c
Alan <alan.kouakou@pharmabox.app>
2022-12-21 14:55:52 +0100

    Initial Commit

---
+++ Chat App/Chat_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chat_AppApp.swift
+//  Chat App
+//
+//  Created by Alan on 21/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Chat_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
